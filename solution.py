cc9fa5a53feb416356dd9a8afc41a4a0690e16d9
Gilbert <gilbertduenas@users.noreply.github.com>
2020-07-27 08:09:15 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,16 @@
+def gradingStudents(grades):
+    rounded = []
 
+    for i in grades:
+        r = i%10
+
+        if i < 38:
+            rounded.append(i)
+        elif r > 2 and r < 5: 
+            rounded.append(i-r+5)
+        elif r > 7 and r < 10:
+            rounded.append(i-r+10)
+        else:
+            rounded.append(i)
+
+    return rounded
